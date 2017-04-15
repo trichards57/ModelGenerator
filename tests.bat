deba40c7cccdc28b5320c0dd936c5e7d831d3753
Tony Richards <trichards57@gmail.com>
2017-04-15 18:21:15 +0100

    Moving back to xunit tests.

--- tests.bat
+++ tests.bat
@@ -5,17 +5,9 @@ mkdir reports\coverage\historydir
 nuget install OpenCover -ExcludeVersion -OutputDirectory tools
 nuget install xunit.runner.console -ExcludeVersion -OutputDirectory tools
 nuget install ReportGenerator -ExcludeVersion -OutputDirectory tools
-nuget install SpecFlow -ExcludeVersion -OutputDirectory tools
-nuget install Pickles.CommandLine -ExcludeVersion -OutputDirectory tools
 
-cd ModelGenerator.Test
-
-..\tools\SpecFlow\tools\specflow.exe generateall ModelGenerator.Test.csproj
-
-cd ..
 
 dotnet build 
 
-tools\OpenCover\tools\OpenCover.Console.exe -target:"tools\xunit.runner.console\tools\xunit.console.x86.exe" -targetargs:"ModelGenerator.Test\bin\Debug\ModelGenerator.Test.dll -noshadow" -register:user -output:"reports\coverage\coverage.xml" -skipautoprops -filter:"+[ModelGenerator]*"  -excludebyattribute:*.ExcludeFromCodeCoverage* -mergebyhash -returntargetcode -coverbytest:*.Test.dll
+tools\OpenCover\tools\OpenCover.Console.exe -target:"tools\xunit.runner.console\tools\xunit.console.x86.exe" -targetargs:"ModelGenerator.Tests\bin\Debug\net46\ModelGenerator.Tests.dll -noshadow" -register:user -output:"reports\coverage\coverage.xml" -skipautoprops -filter:"+[ModelGenerator]*"  -excludebyattribute:*.ExcludeFromCodeCoverage* -mergebyhash -returntargetcode -coverbytest:*.Tests.*
 tools\ReportGenerator\tools\ReportGenerator.exe -reports:reports\coverage\coverage.xml -targetdir:reports\coverage -historydir:reports\coverage\history
-tools\Pickles.CommandLine\tools\pickles.exe --feature-directory=.\ModelGenerator.Test --output-directory=.\docs
